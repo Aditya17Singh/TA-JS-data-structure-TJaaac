0847d85d858e468c00b59b585617f1b21f4dc944
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-05-10 22:18:14 +0530

    array iteration

--- block-BJaacf/code/index.js
+++ block-BJaacf/code/index.js
@@ -1,19 +1,45 @@
 // 1. Create an array named numbers and store 5 number values in it
-
+let numbers = [1,2,3,4,5];
 // 2. Calculate the sum of array items and print it to the console using console.log()
-
+let sum = 0
+for(let i = 0; i < numbers.length; i++){
+    sum = sum + numbers[i];
+}
+console.log(sum);
 // 3. Calculate the average of array items and print it to the console using console.log()
-
+let sum1 = 0
+for(let i = 0; i < numbers.length; i++){
+    sum1 = sum1 + numbers[i];
+}
+console.log(sum1/5);
 // 4. Find the highest number in the array and print it to the console using console.log()
-
+let highest = 0
+for(let i = 0; i < numbers.length; i++){
+    if(highest < numbers[i]){
+        highest = numbers[i];
+    }
+}
+console.log(highest);
 // 5. Find the lowest number in the array and print it to the console using console.log()
-
+let lowest = Infinity
+for(let i = 0; i < numbers.length; i++){
+    if(lowest > numbers[i]){
+        lowest = numbers[i]
+    }
+}
+console.log(lowest);
 // 6. Find the even numbers in the array and print them to the console using console.log()
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 === 0){
+      
+    }
+}
 
 // 7. Find the odd numbers in the array and print them to the console using console.log()
-
+q
 // 8. Find the numbers in the array that is divisible by 5 and print them to the console using console.log()
-
+q
 // 9. Log all the element of the array one by one
-
+q
 // 10. Find all the number in the array that is divisible by 3
+q
